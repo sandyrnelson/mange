80d3b34223d662b48aefc0e5a61a368bd043da3e
Sandy Nelson <sandy.nelson@nexius.hu>
2025-02-27 13:10:37 +0100

    create login form component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,66 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
+import FormInput from '../../ui/FormInput/FormInput';
+import { signInAuthUserWithEmailAndPassword } from '../../utils/firebaseconfig';
+
+const defaultFormFields = {
+	email: '',
+	password: '',
+};
 
 function Login() {
-	return <div>Login</div>;
+	const [formFields, setFormFields] = useState(defaultFormFields);
+	const { email, password } = formFields;
+
+	const resetFormFields = () => {
+		setFormFields(defaultFormFields);
+	};
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+
+		try {
+			await signInAuthUserWithEmailAndPassword(email, password);
+			resetFormFields();
+		} catch (error) {
+			console.log('user sign in failed', error);
+		}
+	};
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+
+		setFormFields({ ...formFields, [name]: value });
+	};
+
+	return (
+		<div className='sign-in-container'>
+			<h2>Already have an account?</h2>
+			<span>Sign in with your email and password</span>
+			<form onSubmit={handleSubmit}>
+				<FormInput
+					label='Email'
+					type='email'
+					required
+					onChange={handleChange}
+					name='email'
+					value={email}
+				/>
+
+				<FormInput
+					label='Password'
+					type='password'
+					required
+					onChange={handleChange}
+					name='password'
+					value={password}
+				/>
+				<div>
+					<button type='submit'>Sign In</button>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default Login;
