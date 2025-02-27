be55fae9f339166b269d9c1dc71d6df5f3acadcc
Sandy Nelson <sandy.nelson@nexius.hu>
2025-02-27 13:11:24 +0100

    adjust home screen with login, sign in, sign out functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
+'use client';
+import { useContext } from 'react';
 import './page.module.css';
+import { UserContext } from '../contexts/UserContext';
+import Login from '../components/Login';
+import SignUpForm from '@/components/SignUpForm/SignUpForm';
+import { signOutUser } from '@/utils/firebaseconfig';
 
 export default function Home() {
+	const { user } = useContext(UserContext);
+
 	return (
 		<div>
-			<div>Home</div>
+			{user ? (
+				<div>
+					Hello {user.displayName}
+					<button className='nav-link' onClick={signOutUser}>
+						SIGN OUT
+					</button>
+				</div>
+			) : (
+				<div>
+					Please log in <Login />
+					<SignUpForm />
+				</div>
+			)}
 		</div>
 	);
 }
