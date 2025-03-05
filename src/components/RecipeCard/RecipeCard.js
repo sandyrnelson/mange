5e59e003b38fe9ac4b79f03a20b57cc20dca739e
Sandy Nelson <sandy.nelson@nexius.hu>
2025-03-05 13:41:42 +0100

    change cook time to total time

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -8,7 +8,7 @@ function RecipeCard({ recipe }) {
 			<RoundedImage src={recipe.thumbnail_url} alt={recipe.name} fallBack='YUM' />
 
 			<div>{recipe.name}</div>
-			<div>Cook time: {recipe.cook_time_minutes} min</div>
+			<div>Cook time: {recipe.total_time_minutes} min</div>
 			<div>{recipe.tags[0].display_name}</div>
 		</div>
 	);
